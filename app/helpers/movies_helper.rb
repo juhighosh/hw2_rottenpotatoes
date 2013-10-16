33df2f306a4ed475731756efb1ec66d31d41610b
Juhi Ghosh <juhi.ghosh@colorado.edu>
2013-10-15 23:35:43 -0600

    Update movies_helper.rb

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,15 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def ratingsInclude(ratingName) #checks clicked box
+    @checked_ratings.include?(ratingName) ? true : false
+  end
+  
+  def createParameterArray(sortColumn) #creates array to allow for sorting
+    @parameterArray = Hash.new
+    @parameterArray.merge!({:ratings => @checked_ratings_hash})
+    @parameterArray.merge!({:sort_by => sortColumn})
+    @parameterArray
+  end
 end
